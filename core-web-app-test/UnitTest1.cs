07bc7328de4891f3a5a89241d41c3d2e0c3b0245
Sachin Patil <Sachin.Patil@blackboard.com>
2018-07-01 08:42:20 -0400

    added web app and test project

---
+++ core-web-app-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace core_web_app_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestUnitTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
